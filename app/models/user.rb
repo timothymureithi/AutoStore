7da461e8416a5b15b3f94ceca81daff0c9fc44d9
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-12-05 23:15:19 +0300

    updated seed file & models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,19 @@
-class User < ApplicationRecord
+class User < ActiveRecord
+    after_create :assign_default_role
+
     rolify 
     has_secure_password 
-    has_many :products, :dependent => :destroy
+    has_many :products, dependent: :destroy
     has_many :orders 
     has_many :order_items, through: :products
     has_many :reviews, dependent: :destroy
     has_one :cart, dependent: :destroy
     validates :first_name, :last_name, :username, :password, presence:true, on: :create
     validates_uniqueness_of :username, :email
+
+    private
+
+    def assign_default_role
+      self.add_role(:buyer) if self.roles.blank?
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,5 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 #User
-# User.create!(email:"timothy@mail.com",password:"tim123",password_confirmation:"tim123",first_name:"Timothy",last_name:"Mureithi")
-# User.create!(email:"judy@mail.com",password:"judy123",password_confirmation:"judy123",first_name:"Judy",last_name:"Muthoni")
+User.create!(email:"timothy@mail.com",password:"tim123",password_confirmation:"tim123",first_name:"Timothy",last_name:"Mureithi")
+User.create!(email:"judy@mail.com",password:"judy123",password_confirmation:"judy123",first_name:"Judy",last_name:"Muthoni")
