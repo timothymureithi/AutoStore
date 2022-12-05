9df7a77478e22ec9d35ae3ef29b9d0b9c64aefd3
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-12-05 21:25:06 +0300

    Added asssociations/user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
     rolify 
     has_secure_password 
-    has_many
+    has_many :products, :dependent => :destroy
+    has_many :orders 
+    has_many :order_items, through: :products
+    has_many :reviews, dependent: :destroy
+    has_one :cart, dependent: :destroy
+    validates :first_name, :last_name, :username, :password, presence:true, on: :create
+    validates_uniqueness_of :username, :email
 end
