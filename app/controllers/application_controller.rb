42486280896007ea12163e30d7256000ad2996f3
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-12-06 01:18:14 +0300

    GET user/:id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+    rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+
+    private
+
+    def record_not_found
+        render json: { errors: "not found" }, status: :not_found
+    end
+
+    def user_not_authorized
+        render json: { errors: "user not authorized" }, status: :unauthorized
+      end
+
 end
