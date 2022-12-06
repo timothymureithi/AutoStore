acea99adca83cc28bb28a2baa77ab4cffb193574
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-12-06 07:05:12 +0300

    added associations for product model

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ gem 'rolify'
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
 
+#gem 'resourcify', '~> 0.1.4'
+
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 gem "bcrypt", "~> 3.1.7"
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,16 @@
 class Product < ApplicationRecord
+    belongs_to :user
+    belongs_to :category
+    has_many :images
+    has_many :cart_products
+    has_many :order_items, dependent: :destroy
+    has_many :reviews
+    has_many :orders, through: :order_items
+    validates :name, :description, :price, :category_id, :color, :quantity, presence: true, on: :create
+
+    
+    def owner
+        user = User.find(self.user_id)
+        UserSerializer.new(user)
+      end
 end
