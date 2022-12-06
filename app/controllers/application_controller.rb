21a230e9efbce3e9ad8d4714d233f6da1575ab2e
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-12-06 07:14:51 +0300

    added cart controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
     rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
     rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+    before_action :authorize 
 
     def current_user
         User.find_by(id: session[:user_id])
+    end
+
+    def current_cart
+        Cart.find_by(id: session[:cart_id])
     end
 
     private
--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,8 @@
 class CartsController < ApplicationController
+
+    def current_cart
+        cart = Cart.find(session[:cart_id])
+        render json: cart
+      end
+    end
 end
